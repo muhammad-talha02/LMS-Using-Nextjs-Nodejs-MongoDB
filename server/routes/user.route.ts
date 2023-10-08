64aa39373ebd00e8908eaecf4c987d7f4ba339f2
Muhammad Talha Saeed <iamtalha971@gmail.com>
2023-10-08 19:06:25 +0400

    Feature -> Activate User using OTP

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { registrationUser } from "../controllers/user.controller";
+import { activateUser, registrationUser } from "../controllers/user.controller";
 const userRouter = express.Router();
 
 
 userRouter.post("/registration" , registrationUser);
+userRouter.post("/activation-user", activateUser)
 
 
 export default userRouter
