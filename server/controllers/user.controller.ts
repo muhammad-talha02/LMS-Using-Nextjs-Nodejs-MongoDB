32ade0da5662513ccd41777fc4c051eb6f9f444c
Muhammad Talha Saeed <iamtalha971@gmail.com>
2023-10-11 22:25:34 +0400

    Feature -> Login &v logout functionality

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -7,6 +7,7 @@ import Jwt, { Secret } from "jsonwebtoken";
 import ejs from "ejs";
 import path from "path";
 import sendMail from "../utils/sendMail";
+import { sendToken } from "../utils/jwt";
 // Register User
 
 interface IRegisterationBody {
@@ -78,7 +79,7 @@ export const createActivationToken = (
       expiresIn: "5m",
     }
   );
-  console.log(token)
+  console.log(token);
   return { token, activationCode };
 };
 
@@ -99,24 +100,77 @@ export const activateUser = catchAsyncError(
         activation_token,
         process.env.ACTIVATION_SECRET_KEY as string
       ) as { user: IUser; activationCode: string };
-      console.log(newUser)
-      if(newUser.activationCode !== activation_code){
-        return next(new ErrorHandler("invalid activation code", 400))
+      console.log(newUser);
+      if (newUser.activationCode !== activation_code) {
+        return next(new ErrorHandler("invalid activation code", 400));
       }
-      const {name, email, password} = newUser.user;
-      const existUser = await userModel.findOne({email});
-      if(existUser){
-        return next(new ErrorHandler("Email already exist", 400))
+      const { name, email, password } = newUser.user;
+      const existUser = await userModel.findOne({ email });
+      if (existUser) {
+        return next(new ErrorHandler("Email already exist", 400));
       }
       const user = await userModel.create({
-        name, email,password
+        name,
+        email,
+        password,
       });
       res.status(200).json({
-        success:true,
-        message:"User Registered Successfully"
-      })
-    } catch (error:any) {
-      return next(new ErrorHandler(error.message, 400))
+        success: true,
+        message: "User Registered Successfully",
+      });
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
     }
   }
 );
+
+// Login User
+
+interface ILoginRequest {
+  email: string;
+  password: string;
+}
+
+export const loginUser = catchAsyncError(
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body as ILoginRequest;
+
+      if (!email || !password) {
+        return next(new ErrorHandler("Please enter email and password", 400));
+      }
+
+      const user = await userModel.findOne({email}).select("+password")
+      console.log(user)
+      if(!user){
+        return next(new ErrorHandler("Email not exist", 400))
+      }
+
+      const isPasswordMatch = await user.comparePassword(password);
+      if(!isPasswordMatch){
+        return next(new ErrorHandler("Invalid password", 400))
+      }
+
+      sendToken(user, 200, res)
+    } catch (error: any) {
+      return next(new ErrorHandler(error.message, 400));
+    }
+  }
+);
+
+
+// logout user
+
+export const logoutUser = catchAsyncError(async(req:Request, res:Response, next:NextFunction)=>{
+  try {
+    res.cookie("access_token", "", {maxAge:1});
+    res.cookie("refresh_token", "", {maxAge:1});
+    res.status(200).json({
+      success:true,
+      message:"Logout Successfully"
+    })
+    
+  } catch (error:any) {
+    return next(new ErrorHandler(error.message, 400))
+  }
+})
