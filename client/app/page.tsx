dc3625061b22f4899d87a33b6675952208ea9ab6
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-01-13 00:15:56 +0400

    Header ---> working.....

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,16 @@
 "use client"
 
-import { FC } from "react"
+import { FC, useState } from "react"
 import Heading from "./utils/Heading"
+import Header from "./components/Header"
 
 
 interface Props {
 
 }
-
-
 const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false)
+  const [activeItem, setActiveItem] = useState(0)
   return (
     <div>
       <Heading
@@ -17,6 +18,7 @@ const Page: FC<Props> = (props) => {
         description="Compile academy is a platform for students to learn and enhance skills."
         keywords="Programming,MERN,Machine Learning"
       />
+      <Header open={open} setOpen={setOpen} activeItem={activeItem} />
     </div>
   )
 }
