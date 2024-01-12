0b5a71f9e86c0a8086ee712cd3144467a3ff45f2
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-01-12 23:42:57 +0400

    Feature -> Next theme configuration

---
+++ client/app/utils/Heading.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from "react";
+
+
+
+interface HeadProps {
+    title: string,
+    description: string,
+    keywords: string
+}
+
+
+const Heading: FC<HeadProps> = (props) => {
+
+    const { title, description, keywords } = props
+
+    return (
+        <>
+            <title>{title}</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <meta name="description" content={description} />
+            <meta name="keywords" content={keywords} />
+        </>
+    )
+}
+
+
+
+export default Heading
