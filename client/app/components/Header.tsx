3dc17e6ea8808d5ac5d3ae190d23cf92d8c405bf
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2024-01-13 17:55:22 +0000

    Header section

--- client/app/components/Header.tsx
+++ client/app/components/Header.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import React, { FC } from 'react'
+import Link from 'next/link'
+import React, { FC, useState } from 'react'
+import NavItems from '../utils/NavItems'
 
 type Props = {
     open: boolean,
@@ -8,12 +10,39 @@ type Props = {
     activeItem: number
 }
 
-const Header: FC<Props> = (props) => {
 
+const Header: FC<Props> = (props) => {
+    const [active, setActive] = useState(false)
+    const [openSidebar, setOpenSidebar] = useState(false)
     const { open, setOpen, activeItem } = props
+
+    if (typeof window !== undefined) {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 80) {
+                setActive(true)
+            }
+            else {
+                setActive(false)
+            }
+        })
+    }
     return (
         <div className='w-full relative'>
-            <div className={`${activeItem ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500" : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"}`}></div>
+            <div className={`${active ? "dark:bg-opacity-50 dark:bg-gradient-to-b dark:from-gray-900 dark:to-black fixed top-0 left-0 w-full h-[80px] z-[80] border-b dark:border-[#ffffff1c] shadow-xl transition duration-500" : "w-full border-b dark:border-[#ffffff1c] h-[80px] z-[80] dark:shadow"}`}>
+                <div className="w-[95%] 800px:w-[92%] m-auto py-0 h-full">
+                    <div className="w-full h-[80px] flex items-center justify-between p-3">
+
+                        <div>
+                            <Link href={"/"} className='text-[25px] font-Poppins font-500 text-black dark:text-white'>
+                                Compile Academy
+                            </Link>
+                        </div>
+                        <div className="flex items-center">
+                            <NavItems activeItem={activeItem} isMobile={false}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
