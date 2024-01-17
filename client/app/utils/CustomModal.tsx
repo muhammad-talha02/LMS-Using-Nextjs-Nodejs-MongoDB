6a15157ba30f0105cf768bae8edf389eec40dcda
Muhammad Talha Saeed <104321361+muhammad-talha02@users.noreply.github.com>
2024-01-17 12:34:45 +0000

    Added -> packges to setup login

---
+++ client/app/utils/CustomModal.tsx
@@ -0,0 +1,23 @@
+import React, { Component, FC } from 'react'
+import { Box, Modal } from "@mui/material"
+type Props = {
+    open: boolean,
+    setOpen: (open: boolean) => void,
+    activeItem: any,
+    Component?: any,
+    setRoute?: (route: string) => void
+}
+
+const CustomModal: FC<Props> = (props) => {
+    const { open, setOpen, activeItem, Component, setRoute } = props
+    return (
+        <Modal open={open} onClose={() => setOpen(false)}>
+            <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 bg-white w-[450px] dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
+
+                <Component setOpen={setOpen} setRoute={setRoute} />
+            </Box>
+        </Modal>
+    )
+}
+
+export default CustomModal
