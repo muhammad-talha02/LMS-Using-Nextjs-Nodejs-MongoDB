5d724d04794e76c14d188a3ebc8c1a99d0257191
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-03-23 22:48:42 +0400

    Popup comonent added

---
+++ client/app/components/Generic/genericStyles.ts
@@ -0,0 +1,12 @@
+export const PopUpStyles:any = {
+    BackDropStyle:'backdrop-blur-[1px] z-[1000] bg-black/60 fixed top-0 left-0 w-full h-full flex justify-center items-center',
+    ModalStyle:'modal text-black dark:text-white dark:bg-[--t-dark-bg] bg-white rounded-sm',
+    ModalSizes:{
+        "sm":"max-w-[400px] w-full",
+        "md":"max-w-[700px] w-full",
+        "lg":"w-[950px]"
+    },
+    ModalHeader:"modalHeader flex justify-between pl-2 pr-1 items-center",
+    ModalBody:'modalBody p-3'
+
+}
