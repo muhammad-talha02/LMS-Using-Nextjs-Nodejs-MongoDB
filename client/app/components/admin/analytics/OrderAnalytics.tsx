7fef45f63380cc9fdbbe69566af91e8a56021021
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-04-10 09:47:13 +0400

    Feature -> order  analytics done

--- client/app/components/admin/analytics/OrderAnalytics.tsx
+++ client/app/components/admin/analytics/OrderAnalytics.tsx
@@ -1,36 +1,44 @@
 import { styles } from '@/app/styles/style'
 import { useGetOrdersAnalyticsQuery } from '@/redux/features/analytics/analyticsApi'
 import React, { FC } from 'react'
-import { AreaChart, Area, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts'
 type Props = {
     isDashboard?: boolean
 }
 
-
-
 const OrderAnalytics: FC<Props> = ({ isDashboard }) => {
-    const { data: courseAnalytics, isLoading } = useGetOrdersAnalyticsQuery({})
+    const { data: ordersAnalyticsData, isLoading } = useGetOrdersAnalyticsQuery({})
 
-    const analyticsData: any = []
-    courseAnalytics && courseAnalytics?.AnalyticsData?.last12Months?.forEach((element: any) => {
-        analyticsData.push({ name: element.month, uv: element.count })
+    const analyticsData: any = [
+        
+    ]
+    ordersAnalyticsData && ordersAnalyticsData?.AnalyticsData?.last12Months?.forEach((element: any) => {
+        analyticsData.push({ name: element.month, count: element.count })
     });
     return (
         <div className={`w-full`}>
             <div>
 
-                <h1 className={`${styles.title} ${isDashboard && "!text-[20px] !text-start"}`}>Users Analytics</h1>
+                <h1 className={`${styles.title} ${isDashboard && "!text-[20px] !text-start"}`}>Orders Analytics</h1>
                 {!isDashboard && <p className='text-center'>Last 12 Months analytics data</p>}
             </div>
             <div className={`w-full mt-5 ${isDashboard ? "h-screen" : "h-[30vh]"}`}>
 
                 <ResponsiveContainer width={isDashboard ? "50%" : "90%"} height={isDashboard ? 225 : 500}>
-                    <AreaChart width={150} height={300} data={analyticsData}>
+                    <LineChart width={500} height={300} data={analyticsData} margin={{
+                        top: 5,
+                        left: 20,
+                        right: 30,
+                        bottom: 5
+                    }}>
+                        <CartesianGrid strokeDasharray={"3 3"} />
                         <XAxis dataKey={"name"} />
                         <YAxis />
-                        <Area dataKey={"count"} type={"monotone"} stroke='#4f62d9' fill='#4f62d9' />
+                        <Tooltip />
+                        {!isDashboard && <Legend />}
+                        <Line dataKey={"count"} type={"monotone"} stroke='#82ca9d' />
 
-                    </AreaChart>
+                    </LineChart>
                 </ResponsiveContainer>
             </div>
         </div>
