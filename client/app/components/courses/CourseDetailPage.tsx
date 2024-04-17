282c790e35d81327a485d85aea1878de74e8e936
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-04-18 00:00:01 +0400

    fixed issues

--- client/app/components/courses/CourseDetailPage.tsx
+++ client/app/components/courses/CourseDetailPage.tsx
@@ -1,30 +1,39 @@
-import Heading from '@/app/utils/Heading'
-import { useGetCourseDetailQuery } from '@/redux/features/courses/coursesApi'
-import React, { FC, useState } from 'react'
-import Header from '../Header'
-import Footer from '../Footer'
-import CourseDetail from './CourseDetail'
+import Heading from "@/app/utils/Heading";
+import { useGetCourseDetailQuery } from "@/redux/features/courses/coursesApi";
+import React, { FC, useState } from "react";
+import Header from "../Header";
+import Footer from "../Footer";
+import CourseDetail from "./CourseDetail";
+import Loader from "../Loader/Loader";
 
 type Props = {
-    courseId: string
-}
+  courseId: string;
+};
 
 const CourseDetailPage: FC<Props> = ({ courseId }) => {
-    const [route, setRoute] = useState("Login")
-    const [open, setOpen] = useState(false)
-    const { data, isLoading } = useGetCourseDetailQuery(courseId, { skip: !courseId })
-    return (
-        <>
-            <Heading
-                title={data?.course.name + " - Compile Academy"}
-                description="Compile academy is a platform for students to learn and enhance skills."
-                keywords={data?.course.tags}
-            />
-            <Header open={open} setOpen={setOpen} activeItem={1} setRoute={setRoute} route={route} />
-            <CourseDetail course={data?.course}/>
-            <Footer />
-        </>
-    )
-}
+  const [route, setRoute] = useState("Login");
+  const [open, setOpen] = useState(false);
+  const { data, isLoading } = useGetCourseDetailQuery(courseId, {
+    skip: !courseId,
+  });
+  return (
+    <>
+      <Heading
+        title={data?.course.name + " - Compile Academy"}
+        description="Compile academy is a platform for students to learn and enhance skills."
+        keywords={data?.course.tags}
+      />
+      <Header
+        open={open}
+        setOpen={setOpen}
+        activeItem={1}
+        setRoute={setRoute}
+        route={route}
+      />
+      {isLoading ? <Loader /> : <CourseDetail course={data?.course} />}
+      <Footer />
+    </>
+  );
+};
 
-export default CourseDetailPage
+export default CourseDetailPage;
