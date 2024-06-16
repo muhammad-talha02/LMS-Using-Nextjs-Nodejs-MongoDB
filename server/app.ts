6c8b4290efb2aed5dae21d5bd319732bd70d439f
Muhammad Talha Saeed <iamtalha971@gmail.com>
2024-06-16 18:45:15 +0400

    add live webiste in cors origin

--- server/app.ts
+++ server/app.ts
@@ -27,7 +27,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1:4000', 'http://localhost:4000'],
+    origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://127.0.0.1:4000', 'http://localhost:4000','https://compile-academy.vercel.app'],
     credentials: true
   })
 );
